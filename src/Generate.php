f2aa3a838baf66e5ada15258a04387ad6abc600d
majiameng <666@majiameng.com>
2019-09-25 18:25:59 +0800

    update qrcode

---
+++ src/Generate.php
@@ -0,0 +1,21 @@
+<?php
+namespace tinymeng\code;
+
+/**
+ * Class Generate.class
+ * @author Tinymeng <666@majiameng.com>
+ * @date: 2019/9/25 18:17
+ */
+class Generate{
+
+    public static function barCode(){
+        echo 123;
+
+    }
+
+
+    public static function qrCode(){
+        echo 123;
+
+    }
+}
